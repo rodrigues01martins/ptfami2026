24ea878d857c37002e2a6e12635cb1aca6785d7f
rodrigues01martins <rodrigues01martins@gmail.com>
2026-04-08 23:59:51 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,9 +2,9 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const config = {
+const firebaseConfig = {
 apiKey: "AIzaSyBPDb_B8dVoyqE76cYeMW5xFuKBFhjHDoE",
-authDomain: "fami2026.firebaseapp.com",
+authDomain: "://firebaseapp.com",
 projectId: "fami2026",
 storageBucket: "fami2026.firebasestorage.app",
 messagingSenderId: "708564516153",
