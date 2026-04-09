5ad7197a8b1170fa081e1e59de8015802585ffe9
rodrigues01martins <rodrigues01martins@gmail.com>
2026-04-09 15:54:29 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,16 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDICljsUW0fO0UJFKeVCsnzW83EbEwlICM",
-  authDomain: "fami2026.firebaseapp.com",
-  projectId: "fami2026",
-  storageBucket: "fami2026.firebasestorage.app",
-  messagingSenderId: "708564516153",
-  appId: "1:708564516153:web:b600235aeff1869f30bce8",
-  measurementId: "G-TCXXDCEQML"
+  apiKey:"AIzaSyBPDb_B8dVoYqE76cYeMW5xFuKBFhjHDoE",
+  authDomain:"fami2026.firebaseapp.com",
+  projectId:"fami2026",
+  storageBucket:"fami2026.firebasestorage.app",
+  messagingSenderId:"708564516153",
+  appId:"1:708564516153:web:28e1c6df35bd660330bce8"
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const app=initializeApp(firebaseConfig);
+export const auth=getAuth(app);
+export const db=getFirestore(app);
