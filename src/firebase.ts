940cb06b0cb01f727accd9eb02636b8369ae6341
rodrigues01martins <rodrigues01martins@gmail.com>
2026-04-09 15:57:52 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey:"AIzaSyBPDb_B8dVoYqE76cYeMW5xFuKBFhjHDoE",
-  authDomain:"fami2026.firebaseapp.com",
-  projectId:"fami2026",
-  storageBucket:"fami2026.firebasestorage.app",
-  messagingSenderId:"708564516153",
-  appId:"1:708564516153:web:28e1c6df35bd660330bce8"
+  apiKey: "AIzaSyBPDb_B8dVoYqE76cYeMW5xFuKBFhjHDoE",
+  authDomain: "fami2026.firebaseapp.com",
+  projectId: "fami2026",
+  storageBucket: "fami2026.firebasestorage.app",
+  messagingSenderId: "708564516153",
+  appId: "1:708564516153:web:28e1c6df35bd660330bce8"
 };
 
 const app=initializeApp(firebaseConfig);
