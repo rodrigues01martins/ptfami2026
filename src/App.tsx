b15a64e6da03616c1010376453f367051391d6d8
rodrigues01martins <rodrigues01martins@gmail.com>
2026-04-22 13:33:21 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,8 @@ import { User as UserIcon } from 'lucide-react';
 
 // SEU UID DE ADMINISTRADOR
 
-export function Ledger({ entries, onEdit, onDelete, onStatusChange, canDelete, isAdmin }: LedgerProps) {
+
+ export function App() { // Removido o conteúdo dos parênteses aqui
   const [user, setUser] = useState<User | null>(null);
   const [isDemoMode, setIsDemoMode] = useState(false);
   const [isAuthReady, setIsAuthReady] = useState(false);
