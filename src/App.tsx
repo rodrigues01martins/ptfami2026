b593159acde4eecf21809dbbf848db7683b3c994
rodrigues01martins <rodrigues01martins@gmail.com>
2026-07-06 15:42:16 -0300

    Add new admin UID to ADMIN_UIDS array

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ export function App() {
   const [toast, setToast] = useState({ message: '', isVisible: false });
   const [editingEntry, setEditingEntry] = useState<LedgerEntry | null>(null);
 
-  const ADMIN_UIDS = ["lba3ydI19fPRDIXF09zXFI7oV8x2", "DfGvSS1g2oPlbLf5y0zazf9LYSx2"];
+  const ADMIN_UIDS = ["lba3ydI19fPRDIXF09zXFI7oV8x2", "DfGvSS1g2oPlbLf5y0zazf9LYSx2", "zTGXyZqsYghjUSfg0ptoEFKiCCc2"];
   const isAdmin = user ? ADMIN_UIDS.includes(user.uid) : false;
 
   useEffect(() => {
